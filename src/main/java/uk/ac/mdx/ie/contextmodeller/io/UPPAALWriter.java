79abce2a0b4ffe07f98b744a5b730cc798f61758
Dean Kramer <d.kramer@mdx.ac.uk>
2016-10-02 21:16:58 +0100

    Provisional Uppaal model generation. More testing needed

--- src/main/java/uk/ac/mdx/ie/contextmodeller/io/UPPAALWriter.java
+++ src/main/java/uk/ac/mdx/ie/contextmodeller/io/UPPAALWriter.java
@@ -16,13 +16,39 @@
 
 package uk.ac.mdx.ie.contextmodeller.io;
 
+import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+
+import org.eclipse.emf.common.util.EList;
+import org.modelio.metamodel.uml.infrastructure.ModelElement;
+import org.modelio.metamodel.uml.statik.AssociationEnd;
+import org.modelio.metamodel.uml.statik.Class;
 import org.modelio.metamodel.uml.statik.Package;
+import org.modelio.vcore.smkernel.mapi.MObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import uk.ac.mdx.ie.contextmodeller.util.ContextStateGroup;
+import uk.ac.mdx.ie.contextmodeller.util.StateTransition;
+import uk.ac.mdx.ie.contextmodeller.util.Utils;
 
 public class UPPAALWriter extends AbstractModelWriter {
 
-	private ArrayList<String> mModelInstanceNames = new ArrayList<String>();
+	private HashMap<String, Set> mAtomicStateGroups = new HashMap<String, Set>();
+	private HashMap<String, Set> mAggregateStateGroups = new HashMap<String, Set>();
+	private HashMap<String, ContextStateGroup> mStateGroups = new HashMap<String, ContextStateGroup>();
+
 
 	@Override
 	public void write(Package model) {
@@ -30,14 +56,202 @@ public class UPPAALWriter extends AbstractModelWriter {
 
 	}
 
+	private static List<MObject> separateAggregateContexts(List<MObject> elements) {
+
+		List<MObject> aggr = new ArrayList<MObject>();
+
+		for (MObject ownedClass : elements) {
+
+			if (((ModelElement) ownedClass).isStereotyped(Utils.CONTEXT_MODELLER, Utils.CONTEXT_STATE)) {
+				EList<AssociationEnd> ends  = ((Class) ownedClass).getTargetingEnd();
+				boolean agg = true;
+
+				if (ends.isEmpty()) {
+					agg = false;
+				}
+
+		    	for (AssociationEnd end : ends) {
+
+		    		MObject assocObject = end.getSource();
+
+		    		if (! ((ModelElement) assocObject).isStereotyped(Utils.CONTEXT_MODELLER, Utils.CONTEXT_STATE)) {
+		    			agg = false;
+		    		}
+		    	}
+
+		    	if (agg) {
+		    		aggr.add(ownedClass);
+		    	}
+			}
+		}
+
+		return aggr;
+	}
+
+	private void generateContextStateGroups() {
+
+		List<MObject> ownedClasses = (List<MObject>) model.getCompositionChildren();
+
+		List<MObject> aggrContexts = separateAggregateContexts(ownedClasses);
+
+		ownedClasses.removeAll(aggrContexts);
+
+		generateContextStateGroup(ownedClasses, true);
+		generateContextStateGroup(aggrContexts, false);
+
+	}
+
+	private void generateContextStateGroup(List<MObject> elements, boolean isAtomic) {
+
+		int i = 1;
+
+		for (MObject ownedClass : elements) {
+
+			ModelElement element = (ModelElement) ownedClass;
+
+			if (element.isStereotyped(Utils.CONTEXT_MODELLER, Utils.CONTEXT_STATE)) {
+				String contextname = ownedClass.getName();
+				String contextGroup = contextname.substring(0, contextname.indexOf("_"));
+				String contextState = contextname.substring(contextGroup.length() + 1);
+
+				ContextStateGroup contextStateGroup = mStateGroups.get(contextGroup);
+
+				if (contextStateGroup == null) {
+					contextStateGroup = new ContextStateGroup(isAtomic, contextGroup, i);
+					mStateGroups.put(contextGroup, contextStateGroup);
+				}
+
+				contextStateGroup.addContextState(contextState);
+				i++;
+			}
+
+		}
+	}
+
 	@Override
 	public String writeToString() {
 
-		StringBuilder sb = new StringBuilder();
-		addHeader(sb);
-		addGlobalDeclarations(sb, null);
+		StringWriter writer = new StringWriter();
 
-		generateQueryFile();
+		try {
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+
+			transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "-//Uppaal Team//DTD Flat System 1.1//EN");
+			transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "http://www.it.uu.se/research/group/darts/uppaal/flat-1_1.dtd");
+
+			Document doc = docBuilder.newDocument();
+			Element root = doc.createElement("nta");
+
+
+			generateContextStateGroups();
+
+			//Global Declarations
+			String contextVariables = "// Place global declarations here.";
+			contextVariables += generateGlobalVariables();
+
+			//StringBuilder sb = new StringBuilder();
+			//addHeader(sb);
+			root.appendChild(addDeclaration(doc, contextVariables));
+
+			root.appendChild(addContextReceiver(doc));
+
+			Element sys = doc.createElement("system");
+			sys.setTextContent(generateSystemDeclaration(null));
+			root.appendChild(sys);
+
+			transformer.transform(new DOMSource(doc), new javax.xml.transform.stream.StreamResult(writer));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+
+		return writer.toString();
+	}
+
+	private void generateTemplates(Document doc, Element root) {
+
+		int idIndex = 0;
+
+		for (ContextStateGroup group : mStateGroups.values()) {
+			Element template = doc.createElement("template");
+
+			Element name = doc.createElement("name");
+			name.setAttribute("x", "5");
+			name.setAttribute("y", "5");
+			name.setTextContent(group.mGroupName);
+			template.appendChild(name);
+
+			Element parameter = doc.createElement("parameter");
+			parameter.setTextContent("urgent chan &amp;newData, urgent chan &amp;deactivate, urgent chan &amp;newAtomicContext");
+			template.appendChild(parameter);
+
+			for (Map.Entry<String, String> entry : group.mStates.entrySet()) {
+
+				Element location = doc.createElement("location");
+				location.setAttribute("id", entry.getValue());
+
+				name = doc.createElement("name");
+				name.setAttribute("x", "5");
+				name.setAttribute("y", "5");
+				name.setTextContent(entry.getKey());
+				location.appendChild(name);
+				template.appendChild(location);
+
+				idIndex++;
+
+			}
+
+			Element init = doc.createElement("init");
+			init.setAttribute("ref", "id" + String.valueOf(idIndex));
+			template.appendChild(init);
+
+			for (StateTransition statechange : group.mTransitions) {
+
+				Element transition = doc.createElement("transition");
+
+				Element transource = doc.createElement("source");
+				transource.setAttribute("ref", statechange.source);
+				transition.appendChild(transource);
+
+				Element trantarget = doc.createElement("target");
+				trantarget.setAttribute("ref", statechange.target);
+				transition.appendChild(trantarget);
+
+				Element label = doc.createElement("label");
+				label.setAttribute("kind", "synchronisation");
+				//label.setAttribute("x", "synchronisation");
+				//label.setAttribute("y", "synchronisation");
+				label.setTextContent(statechange.synch);
+				transition.appendChild(label);
+
+				label = doc.createElement("label");
+				label.setAttribute("kind", "assignment");
+				//label.setAttribute("x", "synchronisation");
+				//label.setAttribute("y", "synchronisation");
+				label.setTextContent(statechange.assign);
+				transition.appendChild(label);
+
+				template.appendChild(transition);
+
+			}
+
+		}
+	}
+
+	private String generateGlobalVariables() {
+
+		StringBuilder sb = new StringBuilder(mStateGroups.size() * 10);
+
+		for (ContextStateGroup group : mStateGroups.values()) {
+			sb.append(System.lineSeparator());
+			sb.append("int ");
+			sb.append(group.mStateVariable);
+			sb.append(" = 0;");
+		}
 
 		return sb.toString();
 	}
@@ -48,63 +262,228 @@ public class UPPAALWriter extends AbstractModelWriter {
 
 	}
 
-	private StringBuilder addGlobalDeclarations(StringBuilder sb, String declarations) {
+	private Element addContextReceiver(Document doc) {
 
-		if (sb == null) {
-			sb = new StringBuilder();
-		}
+		Element contextReceiver = doc.createElement("template");
 
-		if (declarations == null) {
-			declarations = new String();
-		}
+		Element name = doc.createElement("name");
+		name.setAttribute("x", "5");
+		name.setAttribute("y", "5");
+		name.setTextContent("ContextReceiver");
+		contextReceiver.appendChild(name);
 
-		if (declarations.isEmpty()) {
-			sb.append("// Place global declarations here.");
-		} else {
-			sb.append(declarations);
-		}
+		Element parameter = doc.createElement("parameter");
+		parameter.setTextContent("urgent chan &amp;newData, urgent chan &amp;deactivate");
+		contextReceiver.appendChild(parameter);
 
-		return sb;
+		Element declarations = addDeclaration(doc, "// Place local declarations here.");
+		contextReceiver.appendChild(declarations);
+
+		//state one
+		Element location = doc.createElement("location");
+		location.setAttribute("id", "id0");
+		location.setAttribute("x", "232");
+		location.setAttribute("y", "-88");
+
+		name = doc.createElement("name");
+		name.setAttribute("x", "222");
+		name.setAttribute("y", "-118");
+		name.setTextContent("Active");
+		location.appendChild(name);
+		contextReceiver.appendChild(location);
+
+		//state two
+		location = doc.createElement("location");
+		location.setAttribute("id", "id1");
+		location.setAttribute("x", "232");
+		location.setAttribute("y", "72");
+
+		name = doc.createElement("name");
+		name.setAttribute("x", "222");
+		name.setAttribute("y", "42");
+		name.setTextContent("Inactive");
+		location.appendChild(name);
+		contextReceiver.appendChild(location);
+
+		//state transitions
+		Element init = doc.createElement("init");
+		init.setAttribute("ref", "id1");
+		contextReceiver.appendChild(init);
+
+		Element transition = doc.createElement("transition");
+
+		//transition source
+		Element transource = doc.createElement("source");
+		transource.setAttribute("ref", "id0");
+		transition.appendChild(transource);
+
+		//transition target
+		Element trantarget = doc.createElement("target");
+		trantarget.setAttribute("ref", "id1");
+		transition.appendChild(trantarget);
+
+		Element label = doc.createElement("label");
+		label.setAttribute("kind", "synchronisation");
+		label.setAttribute("x", "280");
+		label.setAttribute("y", "-16");
+		label.setTextContent("deactivate!");
+		transition.appendChild(label);
+
+		Element nail = doc.createElement("nail");
+		nail.setAttribute("x", "222");
+		nail.setAttribute("y", "-8");
+		transition.appendChild(nail);
+
+		contextReceiver.appendChild(transition);
+
+
+		//state transitions
+		transition = doc.createElement("transition");
+
+		//transition source
+		transource = doc.createElement("source");
+		transource.setAttribute("ref", "id0");
+		transition.appendChild(transource);
+
+		//transition target
+		trantarget = doc.createElement("target");
+		trantarget.setAttribute("ref", "id0");
+		transition.appendChild(trantarget);
+
+		label = doc.createElement("label");
+		label.setAttribute("kind", "synchronisation");
+		label.setAttribute("x", "280");
+		label.setAttribute("y", "-176");
+		label.setTextContent("newData!");
+		transition.appendChild(label);
+
+		nail = doc.createElement("nail");
+		nail.setAttribute("x", "288");
+		nail.setAttribute("y", "-152");
+		transition.appendChild(nail);
+
+		nail = doc.createElement("nail");
+		nail.setAttribute("x", "184");
+		nail.setAttribute("y", "-152");
+		transition.appendChild(nail);
+		contextReceiver.appendChild(transition);
+
+
+		//state transitions
+		transition = doc.createElement("transition");
+
+		//transition source
+		transource = doc.createElement("source");
+		transource.setAttribute("ref", "id1");
+		transition.appendChild(transource);
+
+		//transition target
+		trantarget = doc.createElement("target");
+		trantarget.setAttribute("ref", "id0");
+		transition.appendChild(trantarget);
+
+		nail = doc.createElement("nail");
+		nail.setAttribute("x", "176");
+		nail.setAttribute("y", "-8");
+		transition.appendChild(nail);
+		contextReceiver.appendChild(transition);
+
+		return contextReceiver;
 	}
 
-	private StringBuilder addSystemDeclarations(StringBuilder sb, String declarations, boolean append) {
+	private Element addDeclaration(Document doc, String declarations) {
+
+		Element declaration = doc.createElement("declaration");
+
+		declaration.appendChild(doc.createTextNode(declarations));
+
+		return declaration;
+	}
+
+	private Element addSelfTransition(Document doc, String ref, String label) {
+
+		Element transition = doc.createElement("transition");
+
+
+		return transition;
+	}
+
+	private Element addTransition(Document doc, Element source, Element target) {
+
+		Element transition = doc.createElement("transition");
+
+		//transition source
+		Element transource = doc.createElement("source");
+		transource.setAttribute("ref", source.getAttribute("ref"));
+		transition.appendChild(transource);
+
+		//transition target
+	    Element trantarget = doc.createElement("target");
+		trantarget.setAttribute("ref", target.getAttribute("ref"));
+		transition.appendChild(trantarget);
+
+
+		Element nail = doc.createElement("nail");
+		nail.setAttribute("x", "176");
+		nail.setAttribute("y", "-8");
+		transition.appendChild(nail);
+
+		return transition;
+
+	}
+
+	private String generateSystemDeclaration(StringBuilder sb) {
 
 		if (sb == null) {
 			sb = new StringBuilder();
 		}
 
-		sb.append("<system>");
-
-		sb.append("// List one or more processes to be composed into a system.");
+		sb.append("urgent chan newData1;");
 		sb.append(System.lineSeparator());
-		sb.append("system ");
+		sb.append("urgent chan deactivate1;");
+		sb.append(System.lineSeparator());
+		sb.append("urgent chan newAtomicContext1;");
+		sb.append(System.lineSeparator());
+		sb.append(System.lineSeparator());
 
-		int numOfItems = mModelInstanceNames.size();
+		//Template instantiation
+		sb.append("// Place template instantiations here.");
+		sb.append(System.lineSeparator());
+		sb.append("Receiver = ContextReceiver(newData1, deactivate1);");
+		sb.append(System.lineSeparator());
 
-		for (int i=0; i < numOfItems; i++) {
-			if (i>0) {
-				sb.append(", ");
+		for (ContextStateGroup group : mStateGroups.values()) {
+			sb.append(group.mInstanceName);
+			sb.append(" = ");
+			sb.append(group.mGroupName);
+
+			if (group.isAtomic) {
+				sb.append("(newData1, deactivate1, newAtomicContext1);");
+			} else {
+				sb.append("(newAtomicContext1);");
 			}
 
-			sb.append(mModelInstanceNames.get(i));
+			sb.append(System.lineSeparator());
 		}
 
-		sb.append(";");
+		sb.append(System.lineSeparator());
 
+		//Last instance list
+		sb.append("// List one or more processes to be composed into a system.");
+		sb.append(System.lineSeparator());
+		sb.append("system Receiver,");
 
-		sb.append("</system>");
-		return sb;
-	}
+		for (ContextStateGroup group : mStateGroups.values()) {
 
-	private StringBuilder addHeader(StringBuilder sb) {
-
-		if (sb == null) {
-			sb = new StringBuilder();
+			sb.append(" ");
+			sb.append(group.mInstanceName);
+			sb.append(",");
 		}
 
-		sb.append("<?xml version=\"1.0\" encoding=\"utf-8\"?><!DOCTYPE nta PUBLIC '-//Uppaal Team//DTD Flat System 1.1//EN' 'http://www.it.uu.se/research/group/darts/uppaal/flat-1_1.dtd'>");
-		sb.append("<nta>");
-		return sb;
+		int lastchar = sb.length();
+		sb.replace(lastchar -1, lastchar, ";");
+
+		return sb.toString();
 	}
 
 }
---
+++ src/main/java/uk/ac/mdx/ie/contextmodeller/util/ContextStateGroup.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016 The ContextModeller Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.mdx.ie.contextmodeller.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ContextStateGroup {
+
+	public HashMap<String, String> mStates;
+	public List<StateTransition> mTransitions;
+	public String mStateVariable;
+	public String mGroupName;
+	public String mInstanceName;
+	public boolean isAtomic;
+
+
+	public ContextStateGroup(boolean atomic, String groupname, int number) {
+		mStates = new HashMap<String, String>();
+		mTransitions = new ArrayList<StateTransition>();
+		groupname = groupname.toLowerCase();
+
+
+		mGroupName = groupname.substring(0, 1).toUpperCase();
+		mGroupName += groupname.substring(1);
+
+		mInstanceName = mGroupName + "Instance";
+		mStateVariable = groupname.substring(0, 1).toLowerCase() + String.valueOf(number);
+	}
+
+
+	public void addContextState(String stateName) {
+		int size = mStates.size();
+
+		String id = "id" + String.valueOf(size);
+
+		int i = 0;
+
+		for (String state : mStates.values()) {
+
+			String number = String.valueOf(i);
+
+			StateTransition trans = new StateTransition();
+
+			trans.source = id;
+			trans.target = "id" + number;
+
+			if (i == 0) {
+				trans.synch = "deactivate?";
+			} else {
+				trans.synch = "newData?newAtomicContext!";
+			}
+
+			trans.assign = mStateVariable + " = " + number;
+
+			mTransitions.add(trans);
+
+			trans = new StateTransition();
+
+			trans.target = id;
+			trans.source = "id" + number;
+			trans.synch = "newData?newAtomicContext!";
+			trans.assign = mStateVariable + " = " + String.valueOf(size);
+
+			mTransitions.add(trans);
+
+		}
+
+		mStates.put(stateName, id);
+
+
+	}
+
+}
